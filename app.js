19a8efac716b0d27838b69f6e7da04cdd823cdea
XTshadowX <vinuthsriarampath@outlook.com>
2024-06-19 10:58:54 +0530

    add a submit button

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-function calc1(){
+function calc(){
     let num1=Number(document.getElementById("num1").value);
     let num2=Number(document.getElementById("num2").value);
+    let op=document.getElementById("op").value;
     lblout=document.getElementById("lblout");
-    let add=num1+num2;
-    
-    lblout.innerHTML=add;
-}
-function calc2(){
-    let num1=Number(document.getElementById("num1").value);
-    let num2=Number(document.getElementById("num2").value);
-    lblout=document.getElementById("lblout");
-    let sub=num1-num2;
-    
-    lblout.innerHTML=sub;
-}
-function calc3(){
-    let num1=Number(document.getElementById("num1").value);
-    let num2=Number(document.getElementById("num2").value);
-    lblout=document.getElementById("lblout");
-    
-    let mul=num1*num2;
-    
-    lblout.innerHTML=mul;
-}
-function calc4(){
-    let num1=Number(document.getElementById("num1").value);
-    let num2=Number(document.getElementById("num2").value);
-    lblout=document.getElementById("lblout");
-    
-    let div=num1/num2;
-    lblout.innerHTML=div;
+    switch(op){
+        case "+":lblout.innerHTML=num1+num2;
+        break;
+        case "-":lblout.innerHTML=num1-num2;
+        break;
+        case "*":lblout.innerHTML=num1*num2;
+        break;
+        case "/":lblout.innerHTML=num1/num2;
+        break;
+    }
 }
